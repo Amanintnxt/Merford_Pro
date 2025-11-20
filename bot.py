17afde4e66d02aa58055afd971b2da6d6fd5b495
Amanintnxt <aman.intnxt@gmail.com>
2025-11-20 18:09:33 +0530

    add gunicorn

--- bot.py
+++ bot.py
@@ -137,4 +137,5 @@ def health_check():
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    app.run(host="0.0.0.0", port=3978)
+    port = int(os.environ.get("PORT", 3978))
+    app.run(host="0.0.0.0", port=port)
